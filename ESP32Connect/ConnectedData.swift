4e98a550558e34bb90a6f1892c8be2fde9884c0f
Martins <m@treiman.s>
2023-11-03 17:19:44 +0200

    Added Basic things and just got ready for the arduino part

---
+++ ESP32Connect/ConnectedData.swift
@@ -0,0 +1,8 @@
+//
+//  ConnectedData.swift
+//  ESP32Connect
+//
+//  Created by Martins Treimanis on 03/11/2023.
+//
+
+import Foundation
---
+++ ESP32Connect/ConnectedView.swift
@@ -0,0 +1,8 @@
+//
+//  ConnectedView.swift
+//  ESP32Connect
+//
+//  Created by Martins Treimanis on 03/11/2023.
+//
+
+import Foundation
